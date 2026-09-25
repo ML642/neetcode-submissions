1e0095bca1466fa081b8774cab50eccb2e0ba82b
Martsin Lazouski <gitforgit123@gmail.com>
2026-09-25 09:22:10 +0200

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*,Node*> mapa;
+
+    Node* dfs(Node* node){
+        if(!node){
+            return nullptr;
+        }
+        if(this->mapa.contains(node)){
+            return this->mapa[node];
+        }
+        
+        Node* copy = new Node(node->val);
+
+        mapa[node] = copy;
+
+        for(auto i:node->neighbors){
+           copy->neighbors.push_back(dfs(i));
+        }
+
+        return copy;
+
+    }
+
+    Node* cloneGraph(Node* node) {
+        return dfs(node);
+    }
+};
