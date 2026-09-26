04160928e4f12549782e7680ab5c52e01de783cf
Martsin Lazouski <gitforgit123@gmail.com>
2026-09-26 11:06:38 +0200

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<string> letters = {
+    "", "", "abc", "def",
+    "ghi", "jkl", "mno",
+    "pqrs", "tuv", "wxyz"
+    };
+    vector<string> answers;
+
+    void dfs(string digits,int current,string ans){
+        if(current==digits.size()){
+            answers.push_back(ans);
+            return;
+        }
+
+        string letters_ = letters[digits[current] - '0'];
+
+        for(auto letter : letters_ ){
+            ans.push_back(letter);
+            dfs(digits,current+1,ans);
+            ans.pop_back();
+        }        
+
+    };
+
+
+
+    vector<string> letterCombinations(string digits) {
+       string s;
+       if(digits.size()==0)return {};
+       dfs(digits,0,s);
+       return this->answers;
+    }
+};
+
