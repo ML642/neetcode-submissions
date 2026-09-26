27d80e37b478f815983544fe359fe7f32e4b2ff4
Martsin Lazouski <gitforgit123@gmail.com>
2026-09-26 09:56:23 +0200

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> answer;
+
+    void dfs(vector<int> nums,int start){
+        if(start == nums.size()){
+        answer.push_back(nums);
+        }
+        for(int i=start;i<nums.size();i++){
+            swap(nums[start],nums[i]);
+            dfs(nums,start+1);
+            swap(nums[start],nums[i]);
+        }
+    }
+    
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> ans{};
+        dfs(nums,0);
+        return this->answer;
+    }
+};
+
